d55e101535bee6b167e485e32ed44404104bb15f
Regeneratel38 <44703575+Regeneratel38@users.noreply.github.com>
2025-01-04 01:06:09 +0545

    Completed LandDetail

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -3,35 +3,47 @@ import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/wrappers/PrivateRoute";
+import LandDetail from "./pages/LandDetail";
 
-export const router = createBrowserRouter ([
-    {
-        path:"/",
-        element:(
-           <PrivateRoute>
-            <Dashboard/>
-            </PrivateRoute>
-            
-        )
-    },
-    {
-        path:"/login",
-        element:(
-            <Login/>
-        )
-    },
-    {
-        path:"/sign-up",
-        element:(
-            <Signup/>
-        )
-    },
-    {
-        path:"/dashboard",
-        element:(
-            <PrivateRoute>
-            <Dashboard/>
-            </PrivateRoute>
-                    )
-    },
-])
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <PrivateRoute>
+        <Dashboard />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/sign-up",
+    element: <Signup />,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <Dashboard />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/land-detail",
+    element: (
+      <PrivateRoute>
+        <LandDetail />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/marketplace",
+    element: (
+      <PrivateRoute>
+        <LandDetail />
+      </PrivateRoute>
+    ),
+  },
+]);
